4368526b8ae3d47a2b84c59cf7647649f5467138
moshi <sh.1692839283.lfs@qq.com>
2019-09-23 14:29:55 +0800

    create lfs fenzhi

--- src/common/config/database.js
+++ src/common/config/database.js
@@ -2,12 +2,12 @@ const mysql = require('think-model-mysql');
 
 module.exports = {
   handle: mysql,
-  database: 'nideshop',
+  database: '1701e-yun',
   prefix: 'nideshop_',
   encoding: 'utf8mb4',
-  host: '220.231.216.128',
+  host: 'localhost',
   port: '3306',
   user: 'root',
-  password: 'Ectriptestdb2018',
+  password: '123123',
   dateStrings: true
 };
